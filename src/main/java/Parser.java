6a80c8eddf03ecd4d4ec2da40b46d06e97f87c05
kechin.vv <kechinv1@rambler.ru>
2021-03-27 17:01:01 +0300

    get projects 1.0

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,2 +1,30 @@
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Parser {
+    public void auth(String user, String password) throws IOException {
+        String url = "https://github.com/search?l=Kotlin&p=2&q=kotlin&type=Repositories";
+        GitHub github = new GitHubBuilder().withPassword(user, password).build();
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        System.out.println(response.toString());
+    }
 }
