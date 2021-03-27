e423e386e5f93efc9c69bb54ced11bac08b3752d
kechin.vv <kechinv1@rambler.ru>
2021-03-27 23:33:09 +0300

    remove obsolete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,9 @@
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        List<String> lines = Files.readAllLines(Paths.get("data.txt"), StandardCharsets.UTF_8);
-        String user = lines.get(0);
-        String password = lines.get(1);
         Parser parser = new Parser();
-        parser.auth(user, password);
+        parser.getPage();
     }
 }
--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,6 +1,4 @@
 import jdk.nashorn.internal.runtime.regexp.joni.Regex;
-import org.kohsuke.github.GitHub;
-import org.kohsuke.github.GitHubBuilder;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,18 +11,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
-    public void auth(String user, String password) throws IOException {
+    public void getPage() throws IOException {
         String url = "https://github.com/search?l=Kotlin&p=2&q=kotlin&type=Repositories";
-        GitHub github = new GitHubBuilder().withPassword(user, password).build();
         URL obj = new URL(url);
         HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
-
         connection.setRequestMethod("GET");
-
         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String inputLine;
-        StringBuffer response = new StringBuffer();
-
+        StringBuilder response = new StringBuilder();
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
         }
@@ -35,8 +29,6 @@ public class Parser {
         Matcher matcher = pattern.matcher(response.toString());
         while (matcher.find())
             href.add(matcher.group());
-
-
         System.out.println(href);
     }
 }
