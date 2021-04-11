999808785ad7c0e05f889613a7980bfb0ffcc6ed
kechin.vv <kechinv1@rambler.ru>
2021-04-12 02:27:04 +0300

    Link class init

---
+++ src/main/java/Main/Link.java
@@ -0,0 +1,23 @@
+package Main;
+
+public class Link {
+    StringBuffer link = new StringBuffer("https://api.github.com/search/repositories?q=kotlin+language:kotlin&sort=stars&order=desc&page=0");
+    StringBuffer link2 = new StringBuffer("https://searchcode.com/api/codesearch_I/?q=get&lan=145&src=2&p=0");
+    int pageIndex = 0;
+    int p = 1;
+
+    public String nextPage() {
+        link.delete(pageIndex, link.length() - 1);
+        p++;
+        link.append(p);
+        return link.toString();
+    }
+
+    private void getPageIndex() {
+        for (int i = link.length() - 1; i > 0; i--)
+            if (link.charAt(i) == '=') {
+                pageIndex = i + 1;
+                break;
+            }
+    }
+}
