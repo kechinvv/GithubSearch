d0f080ea7c51752e08fbc7beaeaa624db3b05b26
kechin.vv <kechinv1@rambler.ru>
2021-06-01 23:15:00 +0300

    get operation sign

--- src/main/java/main/Checker.kt
+++ src/main/java/main/Checker.kt
@@ -1,37 +1,23 @@
 package main
 
+
+import com.intellij.psi.PsiBinaryExpression
+import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
-import org.jetbrains.kotlin.backend.common.descriptors.explicitParameters
-import org.jetbrains.kotlin.backend.common.descriptors.isSuspend
-import org.jetbrains.kotlin.builtins.isBuiltinFunctionalClassDescriptor
-import org.jetbrains.kotlin.cfg.pseudocode.containingDeclarationForPseudocode
-import org.jetbrains.kotlin.codegen.arity
-import org.jetbrains.kotlin.codegen.coroutines.isSuspensionPoint
-import org.jetbrains.kotlin.codegen.coroutines.replaceSuspensionFunctionWithRealDescriptor
-import org.jetbrains.kotlin.descriptors.impl.referencedProperty
-import org.jetbrains.kotlin.ir.expressions.typeParametersCount
-import org.jetbrains.kotlin.js.resolve.diagnostics.JsCallChecker.Companion.isJsCall
+import org.jetbrains.kotlin.KtNodeTypes
+import org.jetbrains.kotlin.KtNodeTypes.BINARY_EXPRESSION
+import org.jetbrains.kotlin.asJava.namedUnwrappedElement
+import org.jetbrains.kotlin.asJava.unwrapped
+import org.jetbrains.kotlin.fir.builder.toFirOperation
+import org.jetbrains.kotlin.fir.builder.toUnaryName
 import org.jetbrains.kotlin.js.resolve.diagnostics.findPsi
 import org.jetbrains.kotlin.psi.*
-import org.jetbrains.kotlin.psi.psiUtil.*
-import org.jetbrains.kotlin.psi.stubs.elements.KtDotQualifiedExpressionElementType
+import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
+import org.jetbrains.kotlin.psi.psiUtil.parentSubstitute
 import org.jetbrains.kotlin.resolve.BindingContext
-import org.jetbrains.kotlin.resolve.bindingContextUtil.getReferenceTargets
-import org.jetbrains.kotlin.resolve.bindingContextUtil.isUsedAsExpression
-import org.jetbrains.kotlin.resolve.calls.callResolverUtil.hasInferredReturnType
 import org.jetbrains.kotlin.resolve.calls.callUtil.*
-import org.jetbrains.kotlin.resolve.calls.inference.ConstraintSystem
-import org.jetbrains.kotlin.resolve.calls.resolvedCallUtil.hasBothReceivers
-import org.jetbrains.kotlin.resolve.calls.smartcasts.getKotlinTypeWithPossibleSmartCastToFP
-import org.jetbrains.kotlin.resolve.calls.tower.isNewNotCompleted
-import org.jetbrains.kotlin.resolve.checkers.ExperimentalUsageChecker.Companion.isExperimentalityAccepted
-import org.jetbrains.kotlin.resolve.descriptorUtil.*
-import org.jetbrains.kotlin.resolve.isGetterOfUnderlyingPropertyOfInlineClass
-import org.jetbrains.kotlin.resolve.jvm.annotations.hasJvmFieldAnnotation
-import org.jetbrains.kotlin.resolve.jvm.annotations.isCallableMemberCompiledToJvmDefault
-import org.jetbrains.kotlin.resolve.multiplatform.couldHaveASource
-import org.jetbrains.kotlin.synthetic.isResolvedWithSamConversions
-import org.jetbrains.kotlin.util.containingNonLocalDeclaration
+import org.jetbrains.kotlin.utils.addToStdlib.cast
+import java.beans.Expression
 
 
 class Checker(bContext: BindingContext, tree: KtFile) {
@@ -41,41 +27,44 @@ class Checker(bContext: BindingContext, tree: KtFile) {
 
     fun checkRecursion(): Boolean {
         val functions = PsiTreeUtil.collectElementsOfType(psi, KtNamedFunction::class.java)
-        functions.forEach { if (it.isRecursive(ctx)) return true  }
+        functions.forEach { if (it.isRecursive(ctx)) return true }
         return false
     }
 
 
     private fun KtNamedFunction.isRecursive(ctx: BindingContext): Boolean {
         val nestedCalls = PsiTreeUtil.collectElementsOfType(this, KtCallExpression::class.java)
-    //    println(nestedCalls)
+        //    println(nestedCalls)
         val resolvedCalls = nestedCalls.mapNotNull { it.getResolvedCall(ctx) }
-    //    println(resolvedCalls)
+        //    println(resolvedCalls)
         val functionDescriptors = resolvedCalls.map { it.resultingDescriptor }
-   //     println(functionDescriptors)
+        //     println(functionDescriptors)
         val functionsPsi = functionDescriptors.mapNotNull { it.findPsi() }
-   //     println(functionsPsi)
+        //     println(functionsPsi)
         return functionsPsi.any { it == this }
-       // return false
+        // return false
     }
 
-    fun checkFieldChange(): Boolean{
+    fun checkFieldChange(): Boolean {
         val functions = PsiTreeUtil.collectElementsOfType(psi, KtNamedFunction::class.java)
-        functions.forEach {if (it.fieldChange()) return true}
+        functions.forEach { if (it.fieldChange()) return true }
         return false
     }
 
     private fun KtNamedFunction.fieldChange(): Boolean {
         val properties = PsiTreeUtil.collectElementsOfType(this, KtQualifiedExpression::class.java)
         println(properties)
-        val propResolve = properties.mapNotNull { it.getParentCall(ctx)}
+        val propResolve = properties.mapNotNull {
+            try {
+                if (it.parent.node.elementType == BINARY_EXPRESSION) it.parent
+                else null
+            } catch (e: AssertionError) {
+                null
+            }
+        }
         println(propResolve)
-        val propDiscr = propResolve.mapNotNull { it}
+        val propDiscr = propResolve.mapNotNull { println((it.node.psi as KtBinaryExpression).operationToken)}
         println(propDiscr)
-       // val test = propResolve.mapNotNull { it. }
-       // val propFinal = propResolve.map { it.getResolvedCall(ctx)}
-      //  println(propFinal)
-      //  return propFinal.any { it != null }
         return false
 
     }
